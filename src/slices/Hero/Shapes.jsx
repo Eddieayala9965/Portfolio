26b4750591d2f848d0e5e8544aae29c3f5678ee0
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-09 16:52:36 -0400

    yo

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -4,8 +4,9 @@ import { Canvas } from "@react-three/fiber";
 import { ContactShadows, Float, Environment } from "@react-three/drei";
 import { Suspense, useEffect, useRef, useState } from "react";
 import { gsap } from "gsap/gsap-core";
+import Model from "./Model";
 
-export function Shapes() {
+function Shapes() {
   return (
     <div className="row-span-1 row-start-1 -mt-9 aspect-square  md:col-span-1 md:col-start-2 md:mt-0">
       <Canvas
@@ -13,10 +14,12 @@ export function Shapes() {
         shadows
         gl={{ antialias: false }}
         dpr={[1, 1.5]}
-        camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}
+        style={{ background: "#03081D" }}
+        camera={{ position: [-5, 1, 10] }}
       >
+        {/* camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }} */}
         <Suspense fallback={null}>
-          <Geometries />
+          <Model />
           <ContactShadows
             position={[0, -3.5, 0]}
             opacity={0.65}
@@ -163,4 +166,5 @@ function Geometry({ r, position, geometry, soundEffects, materials }) {
   );
 }
 export default Shapes;
+
 export { Geometries };
