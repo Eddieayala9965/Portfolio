e28246cf0ab9515dbd9f39f2941aa1f3c08dcedd
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-11 02:12:23 -0400

    about page addded, going to use this avatar 3d model vs what im using now

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { KeyTextField, LinkField } from "@prismicio/client";
+
+type ButtonProps = {
+  linkField: LinkField;
+  label: KeyTextField;
+  showIcon?: boolean;
+  className?: string;
+};
+
+const Button = ({linkField, label, showIcon, className}: ButtonProps) => {
+  return (
+   
+  );
+}
+
+export default Button;
