def3746f0c9450bcf7912f267f6479054a9e2ab2
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-11 23:13:00 -0400

    added the about page

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -3,6 +3,8 @@ import { SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { PrismicRichText } from "@prismicio/react";
+import Button from "@/components/Button";
+import Avatar from "@/components/Avatar";
 
 /**
  * Props for `Biography`.
@@ -25,6 +27,16 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
         <div className="prose prose-xl prose-slate prose-invert col-start-1">
           <PrismicRichText field={slice.primary.description} />
         </div>
+
+        <Button
+          linkField={slice.primary.button_link}
+          label={slice.primary.button_text}
+        />
+
+        <Avatar
+          image={slice.primary.avatar}
+          className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"
+        />
       </div>
     </Bounded>
   );
