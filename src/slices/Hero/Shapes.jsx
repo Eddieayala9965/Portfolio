cc2c3d4f967562b72f6bd0b267b67dd8d1cc870c
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-13 11:00:38 -0400

    going to dev icons per row

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -21,7 +21,7 @@ function Shapes() {
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={4} />{" "}
         <Suspense fallback={null}>
-          <Avatar />
+          <Model />
           <Environment preset="sunset" />
         </Suspense>
       </Canvas>
--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,5 +1,8 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import Heading from "@/components/Heading";
+import React from "react";
+import { MdDeveloperMode } from "react-icons/md";
 
 /**
  * Props for `TechList`.
@@ -15,7 +18,34 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for tech_list (variation: {slice.variation}) Slices
+      <Heading size="lg" as="h2">
+        {slice.primary.heading}
+      </Heading>
+      {slice.items.map(({ tech_color, tech_name }, index) => {
+        return (
+          <div
+            key={index}
+            className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+          >
+            {tech_name} - {tech_color}
+            {Array.from({ length: 15 }, (_, index) => (
+              <React.Fragment key={index}>
+                <span
+                  className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                  style={{
+                    color: index === 7 && tech_color ? tech_color : "inherit",
+                  }}
+                >
+                  {tech_name}
+                </span>
+                <span className="text-3xl">
+                  <MdMosque />
+                </span>
+              </React.Fragment>
+            ))}
+          </div>
+        );
+      })}
     </section>
   );
 };
